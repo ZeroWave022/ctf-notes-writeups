295896f536bb46d0d3c16fa62fc0504da68211d9
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-06-08 23:51:38 +0200

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>CTF Notes & Writeups</span>,
+  project: {
+    link: "https://github.com/ZeroWave022/ctf-notes-writeups",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/ZeroWave022/ctf-notes-writeups",
+  footer: {
+    text: "Nextra Docs",
+  },
+};
+
+export default config;
